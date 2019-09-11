ecbec64bab32eb12aaa6a5a142d57bfbf9432400
Barbara Muesing <muesingb@gmail.com>
2019-09-11 18:21:07 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,20 +38,100 @@ return_array = []
 return return_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
 i = 0
-return_array = [starting_point]
+total = starting_point
   while i < source_array.length do
-    return_array[] = (source_array[i] + return_array[0])
+    total = total + source_array[i]
     i += 1
   end
-return return_array
+return total
 end
 
 def reduce_to_all_true(source_array)
-
+  i = 0
+    while i < source_array.length do
+      if source_array[i] = true
+        return true
+      elsif source_array[i] = false
+        return false
+      i += 1
+      end
+  end
 end
 
 def reduce_to_any_true(source_array)
-
+i = 0
+  if
+    while i < source_array.length do
+      source_array[i] = true
+      i += 1
+    end
+    return true
+  else
+    return false
+  end
 end
+
+
+#i = 0
+#  while i < source_array.length do
+#    if source_array[i] = true
+#      return true
+#    elsif source_array[i] = false
+#      return false
+#    i += 1
+#    end
+#  end
+
+
+# i = 0
+# value == "true"
+#   while i < source_array.length do
+#     if source_array[i] = true
+#       value == "true"
+#     else
+#       value == "false"
+#     i += 1
+#     end
+#   end
+# return value
+
+
+
+
+#i = 0
+#  if
+#    while i < source_array.length do
+#      source_array[i] = true
+#      i += 1
+#    end
+#    return true
+#  else
+#    return false
+#  end
+
+#i = 0
+#value
+#  if
+#    while i < source_array.length do
+#      source_array[i] = true
+#      i += 1
+#    end
+#    value = true
+#  else
+#    value = false
+#  end
+# return value
+
+
+#i = 0
+#    if
+#      while source_array[i] do
+#        value = true
+#        i += 1
+#      end
+#    else
+#      value = false
+#    end
+#return value
